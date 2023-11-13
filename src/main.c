7fdc7fa790bbf4cd75c305266f9f1de01af7134d
w-r d-a <wiktor.skc@gmail.com>
2023-11-13 19:39:29 +0100

    update: multiple messages can be sent with one connection

--- src/main.c
+++ src/main.c
@@ -3,14 +3,19 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <string.h>
 
 const int PORT = 8080;
 
-void handle_connect(int conn_fd) {
-    char buf[1024];
-    read(conn_fd, buf, 1024);
-    printf("%s", buf);
+void handle_connect(int conn_fd, char* addr) {
+    char buf[1024] = {'\0'};
+    int len = read(conn_fd, buf, 1024);
+    if (len > 2) {
+        printf("Info: %s sent message %s\n", addr, buf);
+        handle_connect(conn_fd, addr);
+    }
     close(conn_fd);
+    printf("Info: %s disconnected.\n", name);
 }
 
 int main() {
@@ -32,22 +37,27 @@ int main() {
         goto end;
     }
 
-    if (listen(serv_fd, 32) < 0) {
+    if (listen(serv_fd, 2) < 0) {
         perror("ERROR: sock bind");
         goto end;
     }
+    printf("Listening to port %d ...\n", PORT);
 
     struct sockaddr_in conn_addr = {0};
 
     unsigned int conn_len = sizeof(conn_addr);
 
-    int conn_fd = accept(serv_fd, (struct sockaddr*)&serv_addr, &conn_len);
+    int conn_fd = accept(serv_fd, (struct sockaddr*)&conn_addr, &conn_len);
     if (conn_fd < 0) {
         perror("ERROR: connection accept");
         close(conn_fd);
         goto end;
     }
-    handle_connect(conn_fd);
+    struct in_addr urip = conn_addr.sin_addr;
+    char name[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &urip, name, INET_ADDRSTRLEN);
+    printf("Info: %s connected.\n", name);
+    handle_connect(conn_fd, name);
 end:
     close(serv_fd);
 }
